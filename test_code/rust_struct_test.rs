d1d2c3c0e37727195be42504833ce5a962c04243
Alexander Ivanov <alehander42@gmail.com>
2025-05-23 12:29:52 +0300

    example: add NonZero case to example: reproduce a bug (Petar/Dimo idea)

--- test_code/rust_struct_test.rs
+++ test_code/rust_struct_test.rs
@@ -1,14 +1,29 @@
+// use std::num::NonZero;
+
 struct TestStruct {
     a: i32,
-    b: i32,
 }
 
-fn test_struct(a: i32, b: i32) -> TestStruct {
-    TestStruct { a: a, b: b }
+fn test_struct(a: i32) -> TestStruct {
+    TestStruct { a: a }
 }
+
+fn number() -> usize {
+    1
+}
+// fn non_zero() -> NonZero<usize> {
+    // NonZero::new(2).unwrap()
+// }
 
 fn main() {
-    let test = test_struct(123, 234);
+    let test = test_struct(123);
+
+    let dummy = test_struct(234);
+
+    let first = number();
+
+    // let i = non_zero();
+    println!("{}", test.a);
 
     std::process::exit(0)
 }
